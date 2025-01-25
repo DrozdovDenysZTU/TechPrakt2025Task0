fd4c405c0b6de79253f5be56460126492658f071
DrozdovDenysZTU <vtk241_ddyu@student.ztu.edu.ua>
2025-01-25 04:11:36 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запит першого числа
+        Console.Write("Enter the first number: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        // Запит другого числа
+        Console.Write("Enter the second number: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        // Обчислення суми
+        double sum = number1 + number2;
+
+        // Виведення результату
+        Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+    }
+}
